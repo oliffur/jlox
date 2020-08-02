97cc2ae8443f0eeab1d03d1ec0b234987f40da56
oliffur <oliffur@gmail.com>
2020-08-02 21:41:46 +0800

    Comments for LoxClass

--- com/craftinginterpreters/lox/LoxClass.java
+++ com/craftinginterpreters/lox/LoxClass.java
@@ -8,30 +8,23 @@ class LoxClass implements LoxCallable{
   final LoxClass superclass;
   private final Map<String, LoxFunction> methods;
 
-  LoxClass(String name, LoxClass superclass,
-           Map<String, LoxFunction> methods) {
+  LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
     this.superclass = superclass;
     this.name = name;
     this.methods = methods;
   }
 
   LoxFunction findMethod(String name) {
-    if (methods.containsKey(name)) {
-      return methods.get(name);
-    }
-  
+    if (methods.containsKey(name)) { return methods.get(name); }
     // recursively look in superclass
-    if (superclass != null) {
-      return superclass.findMethod(name);
-    }
-
+    if (superclass != null) { return superclass.findMethod(name); }
     return null;
   }
 
+  /*** CONSTRUCTOR METHODS / LOXCALLABLE METHODS ***/
+  
   @Override
-  public String toString() {
-    return name;
-  }
+  public String toString() { return name; }
 
   @Override
   public Object call(Interpreter interpreter, List<Object> arguments) {
