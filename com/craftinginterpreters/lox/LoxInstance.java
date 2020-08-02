a8637e056911ed6989bb5df1bf46671b8728150c
oliffur <oliffur@gmail.com>
2020-08-02 22:25:19 +0800

    Comments for LoxInstance

--- com/craftinginterpreters/lox/LoxInstance.java
+++ com/craftinginterpreters/lox/LoxInstance.java
@@ -7,28 +7,21 @@ class LoxInstance {
   private LoxClass klass;
   private final Map<String, Object> fields = new HashMap<>();
 
-  LoxInstance(LoxClass klass) {
-    this.klass = klass;
-  }
+  LoxInstance(LoxClass klass) { this.klass = klass; }
 
   Object get(Token name) {
-    if (fields.containsKey(name.lexeme)) {
-      return fields.get(name.lexeme);
-    }
+    // instance.field
+    if (fields.containsKey(name.lexeme)) { return fields.get(name.lexeme); }
 
+    // instance.method()
     LoxFunction method = klass.findMethod(name.lexeme);
     if (method != null) return method.bind(this);
 
-    throw new RuntimeError(name, 
-        "Undefined property '" + name.lexeme + "'.");
+    throw new RuntimeError(name, "Undefined property '" + name.lexeme + "'.");
   }
 
-  void set(Token name, Object value) {
-    fields.put(name.lexeme, value);
-  }
+  void set(Token name, Object value) { fields.put(name.lexeme, value); }
 
   @Override
-  public String toString() {
-    return klass.name + " instance";
-  }
+  public String toString() { return klass.name + " instance"; }
 }
